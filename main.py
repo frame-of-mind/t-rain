bf422bc515d0b348cecd57e16ae1990f4ff8533f
ms-saep <michele.strambi@saep-ict.it>
2019-10-31 17:44:13 +0100

    aggiunta form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__, static_url_path='/static')
 host = os.getenv('HOST', '0.0.0.0')
@@ -8,8 +8,14 @@ host = os.getenv('HOST', '0.0.0.0')
 
 @app.route('/')
 def index():
-    # name = request.args.get("name", "World")
     return app.send_static_file('index.html')
+
+
+@app.route('/hour', methods=['POST'])
+def hour():
+    print(request.form.to_dict(flat=True))
+    # save to db
+    return request.form.to_dict(flat=True), 200
 
 
 # Start the API
