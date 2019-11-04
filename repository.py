0cace8e0822de5e6e2897a2846a789cf20533bc8
mich <michele.strambi@gmail.com>
2019-11-04 21:19:04 +0100

    added username and password to mongodb

--- repository.py
+++ repository.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 # Connect to the MongoDB, change the connection string per your MongoDB environment
 #client = MongoClient(port=27017)
-client = MongoClient('mongodb://ds241968.mlab.com:41968/heroku_s34x7260')
+client = MongoClient('mongodb://root:r00tr00t@ds241968.mlab.com:41968/heroku_s34x7260')
 # Set the db object to point to the business database
 db = client.train_mongo
 trains = db.trains
