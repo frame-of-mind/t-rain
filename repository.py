ca5cb1ec7c322abd1970cd5f925275ccaf3828ff
mich <michele.strambi@gmail.com>
2019-11-04 21:15:21 +0100

    targetting remote mongodb

--- repository.py
+++ repository.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 
 # Connect to the MongoDB, change the connection string per your MongoDB environment
-client = MongoClient(port=27017)
+#client = MongoClient(port=27017)
+client = MongoClient('mongodb://ds241968.mlab.com:41968/heroku_s34x7260')
 # Set the db object to point to the business database
 db = client.train_mongo
 trains = db.trains
