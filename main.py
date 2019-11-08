7cc3913920e7d4a7d459b9d39b7cae584afffb0c
ms-saep <michele.strambi@saep-ict.it>
2019-11-08 12:53:38 +0100

    refactoring

--- main.py
+++ main.py
@@ -19,16 +19,23 @@ def get_all():
 
 @app.route('/logs')
 def get_logs():
+    # TODO: implement filters
     # date
     # user
     # stations
-    resultList = repository.get_all()
+    resultList = repository.get_logs()
     data = {"data": resultList}
     return data, 200
 
 
+@app.route('/train/<id>')
+def get_train(id):
+    return repository.get_train_by_id(id), 200
+
+
 @app.route('/trains')
 def get_trains():
+    # TODO: implement filters
     # from - to
     # starting or arriving
     resultList = repository.get_all()
@@ -39,19 +46,21 @@ def get_trains():
 @app.route('/log', methods=['POST'])
 def add_log():
     req = request.form.to_dict(flat=True)
+    # save train {from, to, should-have-started, should-have-arrived, count}
     train = {
         "from": req.get('from'),
         "to": req.get('to'),
         "start": req.get('should-have-started'),
         "arrive": req.get('should-have-arrived'),
     }
-    repository.save_train(train)
-    # save train {from, to, should-have-started, should-have-arrived, count}
+    train_res = repository.save_train(train)
     # save log {train_id, started, arrived}
-    train = request.form.to_dict(flat=True)
-    print(request.form.to_dict(flat=True))
-    # save to db
-    train_id = repository.insert(train)
+    log = {
+        "train_id": train_res['_id'],
+        "started": req.get('started'),
+        "arrived": req.get('arrived'),
+    }
+    log_res = repository.save_log(log)
     return request.form.to_dict(flat=True), 200
 
 
--- repository.py
+++ repository.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from pymongo import MongoClient
 
 from settings import MONGODB_URI
@@ -18,33 +19,50 @@ logs = db.logs
 
 
 def save_train(train):
-    trains = get_trains(train['from'], train['to'], train['started'], train['arrived'])
-    if len(trains) == 0:
+    found_trains = get_trains(train['from'], train['to'], train['start'], train['arrive'])
+    if len(found_trains) == 0:
         train['count'] = 1
         res = trains.insert_one(train)
     else:
         best_train = None
-        for t in trains:
+        for t in found_trains:
             if best_train is None or t['count'] > best_train['count']:
                 best_train = t
         best_train['count'] += 1
-        res = trains.update_one({'_id': best_train['_id']}, best_train)
+        res = trains.update_one({'_id': ObjectId(best_train['_id'])}, {"$set": {'count': best_train['count']}})
     return res
 
 
 def get_trains(from_station=None, to_station=None, starting_h=None, arriving_h=None):
-    if from_station is None and to_station is None and starting_h is None and arriving_h is None:
-        return
     query = {}
     if from_station is not None:
         query['from'] = from_station
     if to_station is not None:
         query['to'] = to_station
     if starting_h is not None:
-        query['starting'] = starting_h
+        query['start'] = starting_h
     if arriving_h is not None:
-        query['arriving'] = arriving_h
+        query['arrive'] = arriving_h
     cursor = trains.find(query)
+    result = []
+    for document in cursor:
+        document['_id'] = str(document['_id'])
+        result.append(document)
+    return result
+
+
+def get_train_by_id(id):
+    train = trains.find_one({'_id': id})
+    return train
+
+
+def save_log(log):
+    res = logs.insert_one(log)
+    return res
+
+
+def get_logs():
+    cursor = logs.find({})
     result = []
     for document in cursor:
         document['_id'] = str(document['_id'])
